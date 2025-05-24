be198a6fc6c083b7d1d975bf640193b0dc2d4718
JuanFuent.es <juan.fuentes.cabrera.89@gmail.com>
2025-05-23 20:42:35 -0600

    Update metadata og:img

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://juanfuent-es.github.io",
-  base: "/alt-me",
+  site: "https://juanfuent-es.github.io/alt-me",
+  base: "/",
   trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
