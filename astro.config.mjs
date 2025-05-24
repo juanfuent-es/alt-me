782754ee40ab76acce4d36ed073ab7fa292b46fe
JuanFuent.es <juan.fuentes.cabrera.89@gmail.com>
2025-05-23 21:07:50 -0600

    Compile: build without '/_astro' string on assetsDir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://juanfuent-es.github.io/alt-me",
-  base: "/",
+  site: "https://juanfuent-es.github.io/",
+  base: "/alt-me",
   trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
     build: {
-      assetsDir: "alt-me"
+      assetsDir: "/alt-me/_astro"
     }
   },
   typescript: {
