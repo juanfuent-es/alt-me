abeb684345dc61113b17f31524275f2c0169e8aa
JuanFuent.es <juan.fuentes.cabrera.89@gmail.com>
2025-05-23 20:26:21 -0600

    Update astro.config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://www.example.com",
-  base: "/alt-me/",
+  site: "https://juanfuent-es.github.io",
+  base: "/alt-me",
   trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
